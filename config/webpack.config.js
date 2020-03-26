a412866e3626cb5066223613d738705ce0db944a
Eduardo Frausto <eduardo.dx98@gmail.com>
2020-03-25 19:29:11 -0600

    BurgerBuilder 40%

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -431,8 +431,10 @@ module.exports = function (webpackEnv) {
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
-                modules: true,
-                localIdentName: '[name]__[local]__[hash:base64:5]'
+                sourceMap: isEnvProduction && shouldUseSourceMap,
+                modules: {
+                  localIdentName: "[name]__[local]___[hash:base64:5]",
+                }
               }),
             },
             // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBulder'
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder></BurgerBuilder>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
