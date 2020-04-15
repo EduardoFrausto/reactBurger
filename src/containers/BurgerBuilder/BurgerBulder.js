23ca853cf94227095e4548a7ebe5726d824a05cf
Eduardo Frausto <eduardo.dx98@gmail.com>
2020-04-14 19:15:45 -0500

    Routing

--- src/containers/BurgerBuilder/BurgerBulder.js
+++ src/containers/BurgerBuilder/BurgerBulder.js
@@ -87,21 +87,17 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
+
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(`${encodeURIComponent(i)}=${encodeURIComponent(this.state.ingredients[i])}`)
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-
-            })
-            .catch(error => {
-
-            })
-            .finally(() => {
-                this.setState({ loading: false, purchasing: false })
-            });
+        queryParams.push(`price=${this.state.totalPrice}`)
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: `?${queryString}`,
+        });
     }
 
 
