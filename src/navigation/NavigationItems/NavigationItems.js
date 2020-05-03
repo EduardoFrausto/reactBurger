d01d551d56d3a1585f9b9f2c11188a5c40f98c3c
Eduardo Frausto <eduardo.dx98@gmail.com>
2020-05-03 15:22:48 -0500

    Auth 1

--- src/navigation/NavigationItems/NavigationItems.js
+++ src/navigation/NavigationItems/NavigationItems.js
@@ -4,12 +4,9 @@ import classes from './NavigationItems.css'
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link='/' exact>
-            BurgerBuilder
-        </NavigationItem>
-        <NavigationItem link='/orders'>
-            Orders
-        </NavigationItem>
+        <NavigationItem link='/' exact>BurgerBuilder</NavigationItem>
+        <NavigationItem link='/orders'>Orders</NavigationItem>
+        <NavigationItem link='/auth'>Auth</NavigationItem>
     </ul>
 );
 
